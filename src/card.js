941c32fe207e1c19a658bb4402720966fac104ba
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-06 18:09:03 -0600

    adds createCard function

---
+++ src/card.js
@@ -0,0 +1,11 @@
+function createCard(id, question, answers, correctAnswer) {
+    let card = {
+        id: id,
+        question: question,
+        answers: answers,
+        correctAnswer: correctAnswer,
+    }
+return card
+}
+
+module.exports = { createCard }; 

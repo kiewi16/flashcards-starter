4d340e969757a5326c7caf94c610e5f80f61d091
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-06 19:32:34 -0600

    refactors evaluateGuess test suite

--- src/card.js
+++ src/card.js
@@ -10,12 +10,10 @@ function createCard(id, question, answers, correctAnswer) {
 return card
 }
 
-function evaluateGuess(card) {
-    for(let i = 0; i < card.answers.length; i++) {
-        if(card.answers[i] === card.correctAnswer) {
-            return "Correct!"
-        } else {return "Incorrect!"}
-    }
+function evaluateGuess(guess, correctAnswer) {
+  if(guess === correctAnswer) {
+    return "Correct!"
+  } else {return "Incorrect!"}
 }; 
 
 module.exports = { createCard, evaluateGuess }; 

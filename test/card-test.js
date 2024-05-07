78c56e30fe2983ae7c23f82eaa74e8b6d14d9b35
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-07 06:41:15 -0600

    adds countCards tests

--- test/card-test.js
+++ test/card-test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-const { createCard, evaluateGuess, createDeck } = require('../src/card');
+const { createCard, evaluateGuess, createDeck, countCards } = require('../src/card');
 
 describe('card', function() {
   it('should be a function', function() {
@@ -71,6 +71,17 @@ describe('deck', function () {
     expect(deck[2]).to.have.property('correctAnswer', "mutator method");
     
   });
-})
 
-module.exports = { createCard, evaluateGuess, createDeck };
+  it('should count how many cards are in the deck', function() {
+    const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+    const card3 = createCard(3, "What type of prototype method di;rectly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const deck = createDeck([card1, card2, card3]);
+    const numOfCards = countCards(deck); 
+
+    expect(numOfCards).to.equal(3)
+
+  });
+});
+
+module.exports = { createCard, evaluateGuess, createDeck, countCards };

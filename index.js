deee324d8250cd4776b7ca113c0052990bbc83e5
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-08 14:01:32 -0600

    adds start game functions and other code required to start game

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // This is where your project starts.
-
 console.log('Your project is running...'); 
+
+const http = require('http');
+let app = http.createServer();
+const { startGame } = require('./src/game');
+
+app.listen(3000, '127.0.0.1', undefined, () => {
+  startGame();
+  app.close();
+});
+console.log('Node server running on port 3000');

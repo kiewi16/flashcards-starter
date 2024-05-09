c9d3f2bff551feb81af2687d555465ebd9961e33
Kim Ewing <pekar.kimberly@gmail.com>
2024-05-09 15:37:58 -0600

    removed evaluateGuess require

--- test/round-test.js
+++ test/round-test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-const { createCard, evaluateGuess } = require('../src/card');
+const { createCard } = require('../src/card');
 const { createDeck } = require('../src/deck');
 const { createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
 
